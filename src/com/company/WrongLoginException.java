fc114ea1fb003e2d8c6bf7f5af3d379e766f3219
dporh <dporhyn@mail.ru>
2022-12-09 09:36:02 +0300

    all task

---
+++ src/com/company/WrongLoginException.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class WrongLoginException extends Exception {
+    private final String text;
+
+    public WrongLoginException() {
+        text = "Неверный логин";
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+        text = message;
+    }
+
+    @Override
+    public String toString() {
+        return "WrongLoginException: " + text;
+    }
+}
---
+++ src/com/company/WrongPasswordException.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class WrongPasswordException extends Exception{
+    private final String text;
+
+    public WrongPasswordException() {
+        text = "Неверный пароль";
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+        text = message;
+    }
+
+    @Override
+    public String toString() {
+        return "WrongPasswordException: "+text;
+    }
+}
